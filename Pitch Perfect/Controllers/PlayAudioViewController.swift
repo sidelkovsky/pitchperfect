53f36ae166fe6a6dd3e6711d45130f310d54695f
Sidelkovsky <sidelkovsky@tuta.io>
2020-08-09 18:45:43 +0200

    Basic controllers structure.

---
+++ Pitch Perfect/Controllers/PlayAudioViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlayAudioViewController.swift
+//  Pitch Perfect
+//
+//  Created by Kirill Sidelkovsky on 09.08.2020.
+//  Copyright © 2020 sidelkovsky.com. All rights reserved.
+//
+
+import UIKit
+
+class PlayAudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
