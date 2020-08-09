aefc779902a09bbf943ae9060b98b18437de3e6e
Sidelkovsky <sidelkovsky@tuta.io>
2020-08-09 14:34:37 +0200

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Kirill Sidelkovsky on 09.08.2020.
+//  Copyright © 2020 sidelkovsky.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
