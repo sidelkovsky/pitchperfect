058e078106d235db9b30940a8d6e114d87777832
Sidelkovsky <sidelkovsky@tuta.io>
2020-08-15 22:41:46 +0200

    Playing Audio

--- Pitch Perfect/Controllers/PlayAudioViewController.swift
+++ Pitch Perfect/Controllers/PlayAudioViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class PlayAudioViewController: UIViewController {
+class PlayAudioViewController: UIViewController, AVAudioPlayerDelegate {
 
+    // MARK: Outlets
+    
     @IBOutlet weak var btnFast: UIButton!
     @IBOutlet weak var btnSlow: UIButton!
     @IBOutlet weak var btnHighPitch: UIButton!
@@ -18,19 +21,49 @@ class PlayAudioViewController: UIViewController {
     @IBOutlet weak var btnReverb: UIButton!
     @IBOutlet weak var btnStop: UIButton!
     
+    // MARK: Variables
+    
     var isPlaying : Bool = false
     var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case fast = 0, slow, highPitch, lowPitch, echo, reverb
+    }
+    
+    // MARK: Alerts
+    
+    struct Alerts {
+        static let DismissAlert = "Dismiss"
+        static let RecordingDisabledTitle = "Recording Disabled"
+        static let RecordingDisabledMessage = "You've disabled this app from recording your microphone. Check Settings."
+        static let RecordingFailedTitle = "Recording Failed"
+        static let RecordingFailedMessage = "Something went wrong with your recording."
+        static let AudioRecorderError = "Audio Recorder Error"
+        static let AudioSessionError = "Audio Session Error"
+        static let AudioRecordingError = "Audio Recording Error"
+        static let AudioFileError = "Audio File Error"
+        static let AudioEngineError = "Audio Engine Error"
+    }
+    
+    // MARK: Overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
         refreshButtons()
         updateUI()
+        setupAudio()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        stopAudio(btnStop as Any)
+        stopAudio()
     }
+    
+    // MARK: UI Functions
     
     // fix to adjust button sizes for smaller screens
     func refreshButtons() {
@@ -61,14 +94,147 @@ class PlayAudioViewController: UIViewController {
         
     }
     
+    func showAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: UI Invoked Actions
+    
     @IBAction func playAudio(_ sender: Any) {
         isPlaying = true
         updateUI()
+        
+        switch(ButtonType(rawValue: (sender as AnyObject).tag)!) {
+            case .slow:
+                playSound(rate: 0.5)
+            case .fast:
+                playSound(rate: 1.5)
+            case .highPitch:
+                playSound(pitch: 1000)
+            case .lowPitch:
+                playSound(pitch: -1000)
+            case .echo:
+                playSound(echo: true)
+            case .reverb:
+                playSound(reverb: true)
+        }
     }
     
-    @IBAction func stopAudio(_ sender: Any) {
+    @IBAction func stopButtonPressed(_ sender: Any) {
         isPlaying = false
         updateUI()
+        stopAudio()
     }
+    
+    // MARK: Audio Playback Functions
+    
+    func setupAudio() {
+        // initialize (recording) audio file
+        do {
+            audioFile = try AVAudioFile(forReading: recordedAudioURL as URL)
+        } catch {
+            showAlert(Alerts.AudioFileError, message: String(describing: error))
+        }
+    }
+    
+    func connectAudioNodes(_ nodes: AVAudioNode...) {
+        for x in 0..<nodes.count-1 {
+            audioEngine.connect(nodes[x], to: nodes[x+1], format: audioFile.processingFormat)
+        }
+    }
+    
+    @objc func stopAudio() {
+        
+        if let audioPlayerNode = audioPlayerNode {
+            audioPlayerNode.stop()
+        }
+        
+        if let stopTimer = stopTimer {
+            stopTimer.invalidate()
+        }
+        
+        isPlaying = false
+        updateUI()
+                        
+        if let audioEngine = audioEngine {
+            audioEngine.stop()
+            audioEngine.reset()
+        }
+    }
+    
+    func playSound(rate: Float? = nil, pitch: Float? = nil, echo: Bool = false, reverb: Bool = false) {
+        
+        // initialize audio engine components
+        audioEngine = AVAudioEngine()
+        
+        // node for playing audio
+        audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attach(audioPlayerNode)
+        
+        // node for adjusting rate/pitch
+        let changeRatePitchNode = AVAudioUnitTimePitch()
+        if let pitch = pitch {
+            changeRatePitchNode.pitch = pitch
+        }
+        if let rate = rate {
+            changeRatePitchNode.rate = rate
+        }
+        audioEngine.attach(changeRatePitchNode)
+        
+        // node for echo
+        let echoNode = AVAudioUnitDistortion()
+        echoNode.loadFactoryPreset(.multiEcho1)
+        audioEngine.attach(echoNode)
+        
+        // node for reverb
+        let reverbNode = AVAudioUnitReverb()
+        reverbNode.loadFactoryPreset(.cathedral)
+        reverbNode.wetDryMix = 50
+        audioEngine.attach(reverbNode)
+        
+        // connect nodes
+        if echo == true && reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, reverbNode, audioEngine.outputNode)
+        } else if echo == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, audioEngine.outputNode)
+        } else if reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, reverbNode, audioEngine.outputNode)
+        } else {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, audioEngine.outputNode)
+        }
+        
+        // schedule to play and start the engine!
+        audioPlayerNode.stop()
+        audioPlayerNode.scheduleFile(audioFile, at: nil) {
+            
+            var delayInSeconds: Double = 0
+            
+            if let lastRenderTime = self.audioPlayerNode.lastRenderTime, let playerTime = self.audioPlayerNode.playerTime(forNodeTime: lastRenderTime) {
+                
+                if let rate = rate {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate) / Double(rate)
+                } else {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate)
+                }
+            }
+            
+            // schedule a stop timer for when audio finishes playing
+            self.stopTimer = Timer(timeInterval: delayInSeconds, target: self, selector: #selector(PlayAudioViewController.stopAudio), userInfo: nil, repeats: false)
+            RunLoop.main.add(self.stopTimer!, forMode: RunLoop.Mode.default)
+        }
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            showAlert(Alerts.AudioEngineError, message: String(describing: error))
+            return
+        }
+        
+        // play the recording!
+        audioPlayerNode.play()
+    }
+    
     
 }
